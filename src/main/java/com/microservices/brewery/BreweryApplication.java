f755daad5cb0bc3d31478d350b82b850e8537156
clayrisse <clayrisse@outlook.com>
2023-03-02 12:53:16 +0100

    first commit

---
+++ src/main/java/com/microservices/brewery/BreweryApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservices/brewery/BreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
