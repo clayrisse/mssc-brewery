32019a8e448beac04ed65f7f4f150342e0c84098
clayrisse <clayrisse@outlook.com>
2023-04-03 21:16:26 +0200

    Added configuration to use lombok with mapstruct and version bug fixed

--- src/main/java/com/microservices/brewery/web/controller/v2/BeerControllerV2.java
+++ src/main/java/com/microservices/brewery/web/controller/v2/BeerControllerV2.java
@@ -4,6 +4,9 @@ import ch.qos.logback.core.BasicStatusManager;
 import com.microservices.brewery.services.v2.BeerServiceV2;
 import com.microservices.brewery.web.model.BeerDto;
 import com.microservices.brewery.web.model.v2.BeerDtoV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,16 +21,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @Validated  //for the @NotNull
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v2/beer")
 public class BeerControllerV2 {
 
     private final BeerServiceV2 beerServiceV2;
 
-    public BeerControllerV2(BeerServiceV2 beerService) {
-        this.beerServiceV2 = beerService;
-    }
+    //We can take this out with the @RequiredArgsConstructor because it has a final variable
+//    public BeerControllerV2(BeerServiceV2 beerService) {
+//        this.beerServiceV2 = beerService;
+//    }
 
     @GetMapping({"/{beerId}"})
     @CrossOrigin()
@@ -37,8 +43,17 @@ public class BeerControllerV2 {
 
     @PostMapping//Post - create new beer
     public ResponseEntity handlePost2(@Valid @NotNull @RequestBody BeerDtoV2 beerDto) {
-        BeerDtoV2 savedDto = beerServiceV2.saveNewBeer(beerDto);
-        HttpHeaders headers = new HttpHeaders();
+
+        //Using @SL4j
+        log.debug("in handle post... ");
+
+
+//        BeerDtoV2 savedDto = beerServiceV2.saveNewBeer(beerDto); //example for val in lombok
+//        HttpHeaders headers = new HttpHeaders(); //example for val in lombok
+
+        val savedDto = beerServiceV2.saveNewBeer(beerDto);
+        val headers = new HttpHeaders();
+
         //todo add hostname
         headers.add("Location","/api/v1/beer/" + savedDto.getId().toString());
         return new ResponseEntity(headers, HttpStatus.CREATED);
