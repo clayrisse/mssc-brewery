6e83722c28f842529dd6b19fe11c908ca3e73fc0
clayrisse <clayrisse@outlook.com>
2023-04-24 21:04:26 +0200

    Playing with json serializer properties with dates

---
+++ src/main/java/com/microservices/brewery/domain/LocalDateSerializer.java
@@ -0,0 +1,17 @@
+package com.microservices.brewery.domain;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateSerializer extends JsonSerializer<LocalDate> {
+
+    @Override
+    public void serialize(LocalDate localDate, JsonGenerator jsonGenerator, SerializerProvider serializer) throws IOException {
+        jsonGenerator.writeObject((localDate.format(DateTimeFormatter.BASIC_ISO_DATE)));
+    }
+}
--- src/main/java/com/microservices/brewery/web/model/BeerDto.java
+++ src/main/java/com/microservices/brewery/web/model/BeerDto.java
@@ -1,5 +1,9 @@
 package com.microservices.brewery.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.microservices.brewery.domain.LocalDateSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +13,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
@@ -18,6 +23,7 @@ import java.util.UUID;
 @Builder
 public class BeerDto {
 
+    @JsonProperty("beerId")// this overwrites the property name to beerId
     @Null
     private UUID id;
 
@@ -30,9 +36,14 @@ public class BeerDto {
     @Positive
     private Long upc;
 
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:Z", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
     private OffsetDateTime lastUpdatedDate;
 
-    //this property is for jackson example
+//    this property price is for jackson example
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private BigDecimal price;
+
+    @JsonSerialize(using = LocalDateSerializer.class)
+    private LocalDate myLocalDate;
 }
